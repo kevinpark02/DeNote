66f05c48cb318785a44d7d00496247996eb64dde
kevpark02 <kpark@underdogmedia.com>
2026-08-16 21:28:54 -0400

    implement time stamp input

--- src/components/Listen.tsx
+++ src/components/Listen.tsx
@@ -1,15 +1,19 @@
 import { useState } from 'react'
 import Dropdown, { DropdownId } from './Dropdown'
 import InputUrl from './InputUrl'
+import TimeInput from './TimeInput'
 import './Listen.css'
 
 const Listen = () => {
   const [activeDropdown, setActiveDropdown] = useState<DropdownId | undefined>(undefined)
   const [url, setUrl] = useState('')
+  const [from, setFrom] = useState('')
+  const [to, setTo] = useState('')
   return (
     <div className="listen">
       <Dropdown activeDropdown={activeDropdown} onChange={setActiveDropdown} />
       <InputUrl url={url} onChange={setUrl} />
+      <TimeInput from={from} to={to} onFromChange={setFrom} onToChange={setTo} />
     </div>
   )
 }
